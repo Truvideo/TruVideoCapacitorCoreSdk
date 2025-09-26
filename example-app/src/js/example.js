c6d2255b3a391ec48dbcdccaf5eab96ca8c0dde5
himanshu-rathore-5exe <himanshu.rathore@5exceptions.com>
2025-09-26 11:59:59 +0530

    Revert "Dev himanshu"

--- example-app/src/js/example.js
+++
@@ -1,6 +0,0 @@
-import { TruVideoSdkCore } from 'truvideo-capacitor-core-sdk';
-
-window.testEcho = () => {
-    const inputValue = document.getElementById("echoInput").value;
-    TruVideoSdkCore.echo({ value: inputValue })
-}
